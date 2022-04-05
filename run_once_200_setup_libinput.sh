4ba7818eb81d6dfd69944dfcf44dccc9de030f5e
username <emailaddress@emailprovider.com>
2022-04-05 18:07:54 +0000

    Changed xorg setup script

--- run_once_200_setup_libinput.sh
+++ run_once_200_setup_libinput.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Delete the folder if it exists
+[ -d "/etc/X11/xorg.conf.d/" ] && sudo rm -rf /etc/X11/xorg.conf.d/
+
 # Copy xorg configs to /etc/X11/xorg.conf.d/
 sudo mkdir /etc/X11/xorg.conf.d/
 sudo ln $HOME/.config/xorg/70-synaptics.conf \
