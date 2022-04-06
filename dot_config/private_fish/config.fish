ee4f45be9074b69f66611bde52c8e7b527cce372
username <emailaddress@emailprovider.com>
2022-04-06 00:23:36 +0000

    Made X autostart after login

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -10,3 +10,10 @@ fish_add_path -p $HOME/bin
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Start X at login
+if status is-login
+    if test -z "$DISPLAY" -a "$XDG_VTNR" = 1
+        exec startx -- -keeptty
+    end
+end
